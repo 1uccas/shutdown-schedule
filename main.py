56f0479615f8e4f0c2b543526c06ea451cd706d2
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-11 19:14:43 -0300

    feat ~ capture value in input with def

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ windows.geometry("400x400")
 windows.title("Shutdown Schedule")
 windows.configure(bg="black")
 
+def capValue():
+    e_text=entry.get()
+    print(e_text)
 
 entry = ct.CTkEntry(master=windows, placeholder_text="Enter your time")
 entry.place(relx=0.5, rely=0.5, anchor=tkr.CENTER)
-state = entry.cget("state")
 
-button = ct.CTkButton(master=windows, corner_radius=10, text="Click")
+button = ct.CTkButton(master=windows, corner_radius=10, text="Click", command=capValue)
 button.place(relx=0.5, rely=0.6, anchor=tkr.CENTER)
+
 
 windows.mainloop()
