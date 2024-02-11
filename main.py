07449096a44752285721e073e8c9cc97dafb724a
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-11 19:13:11 -0300

    feat ~ add new inputText

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ windows.geometry("400x400")
 windows.title("Shutdown Schedule")
 windows.configure(bg="black")
 
-button = ct.CTkButton(master=windows, corner_radius=10, text="Click")
-button.place(relx=0.5, rely=0.5, anchor=tkr.CENTER)
 
+entry = ct.CTkEntry(master=windows, placeholder_text="Enter your time")
+entry.place(relx=0.5, rely=0.5, anchor=tkr.CENTER)
+state = entry.cget("state")
+
+button = ct.CTkButton(master=windows, corner_radius=10, text="Click")
+button.place(relx=0.5, rely=0.6, anchor=tkr.CENTER)
 
 windows.mainloop()
