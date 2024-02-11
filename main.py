2fac5ce6c093b974db1ec9da9d4d7e1e42e4ed42
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-11 18:33:50 -0300

    feat ~ add new background color dark in windows

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import tkinter as tkr
-import customtkinter  as ct
+import customtkinter as ct
 
 windows = tkr.Tk() 
 windows.geometry("400x400")
 windows.title("Shutdown Schedule")
-
+windows.configure(bg="black")
 
 button = ct.CTkButton(master=windows, corner_radius=10, text="Click")
 button.place(relx=0.5, rely=0.5, anchor=tkr.CENTER)
+
 
 windows.mainloop()
