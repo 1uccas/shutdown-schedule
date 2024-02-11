62ca11ea16762fcacabfaaa96e44fa7f3578e342
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-11 18:19:13 -0300

    feat ~ add new display with customtkinter

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tkr
+import customtkinter  as ct
+
+windows = tkr.Tk() 
+windows.geometry("400x400")
+windows.title("Shutdown Schedule")
+
+
+button = ct.CTkButton(master=windows, corner_radius=10, text="Click")
+button.place(relx=0.5, rely=0.5, anchor=tkr.CENTER)
+
+windows.mainloop()
