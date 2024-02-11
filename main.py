ce751b2fb72adcdea082efe4b493f3d6dec403db
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-11 20:36:00 -0300

    feat ~ add new functions -> exitWindows, TurnOff
    * - Both functions were assigned to the buttons

--- main.py
+++ main.py
@@ -7,21 +7,28 @@ windows.geometry("400x400")
 windows.title("Shutdown Schedule")
 windows.configure(bg="black")
 
+def exitWindows():
+    exit()
+
+def TurnOff():
+    os.system(f"shutdown -a")
+    print("Finish Shutdown")
+
 def capValue():
     try:
         e_text=int(entry.get())
         valueConvert = int(e_text * 60)
-        #os.system(f"shutdown -s -t {valueConvert}")
+        os.system(f"shutdown -s -t {valueConvert}")
         print(e_text)
         
         new_windows = ct.CTkToplevel()
         new_windows.geometry("400x400")
         new_windows.title("New Windows")
         
-        buttonExit = ct.CTkButton(master=new_windows, corner_radius=10, text="Exit windows")
+        buttonExit = ct.CTkButton(master=new_windows, corner_radius=10, text="Exit windows", command=exitWindows)
         buttonExit.place(relx=0.3, rely=0.5, anchor=tkr.CENTER)
         
-        buttonTurnOff = ct.CTkButton(master=new_windows, corner_radius=10, fg_color="darkred", text="Turn Off Shutdown")
+        buttonTurnOff = ct.CTkButton(master=new_windows, corner_radius=10, fg_color="darkred", text="Turn Off Shutdown", command=TurnOff)
         buttonTurnOff.place(relx=0.7, rely=0.5, anchor=tkr.CENTER)
         
     except ValueError as Error:
